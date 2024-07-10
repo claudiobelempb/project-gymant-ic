e7c27d03f3015db2b7311e987751ec7d4be9df27
claudiobelempb <claudio.c.lima@hotmail.com>
2024-07-10 17:36:20 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Home Page</h1>
-    </main>
+    <>
+      <header className='header'>
+        <h1>Header</h1>
+      </header>
+      <main className='container'>
+        <section className='showcase'>
+          <h1>Sow Case</h1>
+        </section>
+        <section className='motto'>
+          <h1>Motto</h1>
+        </section>
+        <section className='features'>
+          <h1>Features</h1>
+        </section>
+        <section className='trainers'>
+          <h1>Trainers</h1>
+        </section>
+        <section className='testimonials'>
+          <h1>Testimonials</h1>
+        </section>
+        <section className='clients'>
+          <h1>Clients</h1>
+        </section>
+        <section className='register'>
+          <h1>Register</h1>
+        </section>
+      </main>
+      <footer className='footer'>
+        <h1>Footer</h1>
+      </footer>
+    </>
   );
 }
